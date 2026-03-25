050af0b0b85b79774752ef544071f719e1dfd51a
Rian Gabriel <riangabrieldev@gmail.com>
2026-03-25 11:02:44 -0300

    feat: add farm CRUD controller

---
+++ src/Controller/FarmController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Farm;
+use App\Form\FarmType;
+use App\Repository\CowRepository;
+use App\Repository\FarmRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/farms', name: 'app_farm_')]
+class FarmController extends AbstractController
+{
+    public function __construct(
+        private FarmRepository $repository,
+    ) {
+    }
+
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(Request $request, PaginatorInterface $paginator): Response
+    {
+        $search = $request->query->get('search');
+        $query = $this->repository->findBySearch($search);
+
+        $farms = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('farm/index.html.twig', [
+            'farms' => $farms,
+            'search' => $search,
+        ]);
+    }
+
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function create(Request $request): Response
+    {
+        $farm = new Farm();
+        $form = $this->createForm(FarmType::class, $farm);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->repository->save($farm, true);
+            $this->addFlash('success', 'Fazenda cadastrada com sucesso.');
+
+            return $this->redirectToRoute('app_farm_index');
+        }
+
+        return $this->render('farm/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Farm $farm): Response
+    {
+        return $this->render('farm/show.html.twig', [
+            'farm' => $farm,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Farm $farm, CowRepository $cowRepository): Response
+    {
+        $form = $this->createForm(FarmType::class, $farm);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $animals = $cowRepository->count(['farm' => $farm]);
+            $limit = (int) floor($farm->getSize() * Farm::MAX_ANIMALS_PER_HECTARE);
+
+            if ($animals > $limit) {
+                $this->addFlash('error', "O tamanho não pode ser reduzido: a fazenda possui {$animals} animais, mas o novo tamanho comporta apenas {$limit}.");
+
+                return $this->render('farm/edit.html.twig', [
+                    'farm' => $farm,
+                    'form' => $form,
+                ]);
+            }
+
+            $this->repository->save($farm, true);
+            $this->addFlash('success', 'Fazenda atualizada com sucesso.');
+
+            return $this->redirectToRoute('app_farm_index');
+        }
+
+        return $this->render('farm/edit.html.twig', [
+            'farm' => $farm,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Farm $farm): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $farm->getId(), $request->request->get('_token'))) {
+            $this->repository->remove($farm, true);
+            $this->addFlash('success', 'Fazenda removida com sucesso.');
+        }
+
+        return $this->redirectToRoute('app_farm_index');
+    }
+}
